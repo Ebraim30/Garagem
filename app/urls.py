e198853f59337f01736645b92cc45385e797b185
Ebraim30 <ebraim.herrera@gmail.com>
2025-05-03 20:18:51 -0300

    feat: acessórios feito

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,12 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
-
-from core.views import UserViewSet
+from core.views import AcessorioViewSet, UserViewSet
+# from core.views import UserViewSet
 
 router = DefaultRouter()
 
+router.register(r'acessorios', AcessorioViewSet, basename='acessorios')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
